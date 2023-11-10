aa777afe46785e8f0dd738a49400caba7548d539
Augusto Neves <augustoneves@frwk.com.br>
2023-11-10 11:59:27 -0300

    style: creating global styles for the application

---
+++ assets/styles/globalStyle.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  backgroundWhite: {
+    backgroundColor: '#FFFFFF',
+  },
+  flex: {
+    flex: 1,
+  },
+  text: {
+    color: '#000000',
+  },
+  headerText: {
+    color: '#022150',
+  },
+});
