50cb3eb09e4e82c7e25b31dde92342cf9920777e
Augusto Neves <augustoneves@frwk.com.br>
2023-11-13 20:30:33 -0300

    feat: adding Redux provider and Persist Gate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
 import {MainNavigation} from './routes/mainNavigation';
+import {Provider} from 'react-redux';
+import {persister, store} from './store/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 function App() {
   return (
-    <NavigationContainer>
-      <MainNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persister} loading={null}>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
 
