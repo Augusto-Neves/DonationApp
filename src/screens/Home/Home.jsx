f672ed1a68f462ded557ccfe204ff1d29e45001f
Augusto Neves <augustoneves@frwk.com.br>
2023-11-14 22:08:01 -0300

    feat: adding components and new features to home screen

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,33 +1,161 @@
-import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import {globalStyles} from '../../../assets/styles/globalStyle';
 import {Header} from '../../components/Header/Header';
 import {Search} from '../../components/Search/Search';
+import {Tab} from '../../components/Tab/Tab';
 import {DonationItem} from '../../components/DonationItem/DonationItem';
 import {styles} from './styles';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {updatedSelectedCategoryId} from '../../store/reducers/Categories';
 
 export function Home() {
+  const [categoryPage, setCategoryPage] = useState(1);
+  const [categoryList, setCategoryList] = useState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+  const [donationItem, setDonationItem] = useState([]);
+
+  const categoryPageSize = 4;
+
   const user = useSelector(state => state.user);
+  const categories = useSelector(state => state.categories);
+  const donations = useSelector(state => state.donations);
+  const dispatch = useDispatch();
+
+  function pagination(items, pageNumber, pageSize) {
+    const startIndex = (pageNumber - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+
+    if (startIndex >= items.length) {
+      return [];
+    }
+
+    return items.slice(startIndex, endIndex);
+  }
+
+  useEffect(() => {
+    const items = donations.items;
+    const filteredItems = items.filter(item =>
+      item.categoryIds.includes(categories.selectedCategoryId),
+    );
+
+    setDonationItem(filteredItems);
+  }, [categories.selectedCategoryId, donations.items]);
+
+  useEffect(() => {
+    setIsLoadingCategories(true);
+    setCategoryList(
+      pagination(categories.categories, categoryPage, categoryPageSize),
+    );
+    setCategoryPage(prev => prev + 1);
+    setIsLoadingCategories(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <SafeAreaView style={[globalStyles.backgroundWhite, globalStyles.flex]}>
-      <Header title={`${user.firstName} ${user.lastName}`} type={1} />
-      <Search onSearch={value => console.log(value)} />
-      <View style={styles.container}>
-        <DonationItem
-          badgeTitle="Environment"
-          donationTitle="Tree Cactus"
-          price={44}
-          uri="https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"
-        />
-        <DonationItem
-          badgeTitle="Environment"
-          donationTitle="Tree Cactus"
-          price={44}
-          uri="https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"
-        />
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.headerText}>Hello, </Text>
+            <View style={styles.userName}>
+              <Header
+                title={`${user.firstName} ${user.lastName[0]}. 👋`}
+                type={1}
+              />
+            </View>
+          </View>
+
+          <Image
+            source={{uri: user.profileImage}}
+            style={styles.profileImage}
+            resizeMode="contain"
+          />
+        </View>
+
+        <View style={styles.searchContainer}>
+          <Search onSearch={value => console.log(value)} />
+        </View>
+
+        <TouchableOpacity
+          style={styles.highlightImageContainer}
+          activeOpacity={0.7}>
+          <Image
+            style={styles.highlightImage}
+            source={require('../../../assets/images/highlighted_image.png')}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+
+        <View style={styles.categoryHeader}>
+          <Header title="Select Category" type={2} />
+        </View>
+
+        <View style={styles.categoriesContainer}>
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={categoryList}
+            renderItem={({item}) => (
+              <View style={styles.categoryItem} key={item.categoryId}>
+                <Tab
+                  tabId={item.categoryId}
+                  title={item.name}
+                  isInactive={item.categoryId !== categories.selectedCategoryId}
+                  onPress={value => dispatch(updatedSelectedCategoryId(value))}
+                />
+              </View>
+            )}
+            onEndReachedThreshold={0.5}
+            onEndReached={() => {
+              if (isLoadingCategories) {
+                return;
+              }
+
+              setIsLoadingCategories(true);
+              let newData = pagination(
+                categories.categories,
+                categoryPage,
+                categoryPageSize,
+              );
+
+              if (newData.length > 0) {
+                setCategoryList(prevState => [...prevState, ...newData]);
+                setCategoryPage(prevState => prevState + 1);
+              }
+              setIsLoadingCategories(false);
+            }}
+          />
+        </View>
+        {donationItem.length > 0 && (
+          <View style={styles.donationItemContainer}>
+            {donationItem.map(donation => (
+              <DonationItem
+                badgeTitle={
+                  categories.categories.filter(
+                    category =>
+                      category.categoryId === categories.selectedCategoryId,
+                  )[0].name
+                }
+                donationTitle={donation.name}
+                donationItemId={donation.donationItemId}
+                price={parseFloat(donation.price)}
+                uri={donation.image}
+                key={donation.donationItemId}
+                onPress={selectedDonationId => console.log(selectedDonationId)}
+              />
+            ))}
+          </View>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,13 +1,57 @@
 import {StyleSheet} from 'react-native';
-import {horizontalScale} from '../../utils/scaling';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../utils/scaling';
+import {getFontFamily} from '../../utils/getFontFamily';
 
 export const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: horizontalScale(24),
+  categoriesContainer: {
+    marginHorizontal: horizontalScale(24),
   },
-  text: {
-    color: 'black',
+  categoryItem: {
+    marginRight: horizontalScale(10),
+  },
+  categoryHeader: {
+    marginHorizontal: horizontalScale(24),
+    marginBottom: verticalScale(16),
+    marginTop: verticalScale(6),
+  },
+  donationItemContainer: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(24),
+  },
+  header: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(24),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontFamily: getFontFamily('Inter', '400'),
+    fontSize: scaleFontSize(16),
+    lineHeight: scaleFontSize(19),
+    letterSpacing: scaleFontSize(0.32),
+    color: '#636776',
+  },
+  highlightImageContainer: {
+    marginHorizontal: horizontalScale(24),
+  },
+  highlightImage: {
+    width: '100%',
+    height: verticalScale(160),
+  },
+  userName: {
+    marginTop: verticalScale(5),
+  },
+  profileImage: {
+    width: horizontalScale(50),
+    height: verticalScale(50),
+  },
+  searchContainer: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(24),
   },
 });
