6ecf1e074e9dd3f92202474523ea861368965e71
Augusto Neves <augustoneves@frwk.com.br>
2023-11-20 19:08:25 -0300

    feat: adding single donation route

--- src/routes/mainNavigation.jsx
+++ src/routes/mainNavigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './routesNames';
 import {Home} from '../screens/Home/Home';
+import {SingleDonation} from '../screens/SingleDonation/SingleDonation';
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ export function MainNavigation() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name={Routes.Home} component={Home} />
+      <Stack.Screen name={Routes.SingleDonation} component={SingleDonation} />
     </Stack.Navigator>
   );
 }
