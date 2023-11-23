b1bd2b77406437ef38e2de34028c8c5d71a9398f
Augusto Neves <augustoneves@frwk.com.br>
2023-11-22 23:28:11 -0300

    feat: refreshing user token based  in app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+import {AppState} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {MainNavigation} from './routes/mainNavigation';
+import {MainNavigation} from './routes/index';
 import {Provider} from 'react-redux';
 import {persister, store} from './store/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import {checkToken} from './api/user';
 
 function App() {
+  const appState = useRef(AppState.currentState);
+
+  useEffect(() => {
+    AppState.addEventListener('change', nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        checkToken();
+      }
+    });
+    checkToken();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persister} loading={null}>
