76feb061a4124eaf1265a068c2c3dfbdc401a005
Augusto Neves <augustoneves@frwk.com.br>
2023-12-01 15:43:11 -0300

    style: adding styles to back button and card form

--- src/screens/Payment/styles.js
+++ src/screens/Payment/styles.js
@@ -2,6 +2,10 @@ import {StyleSheet} from 'react-native';
 import {horizontalScale, verticalScale} from '../../utils/scaling';
 
 export const styles = StyleSheet.create({
+  backButtonContainer: {
+    marginTop: verticalScale(12),
+    marginBottom: verticalScale(24),
+  },
   button: {
     marginHorizontal: horizontalScale(24),
     marginBottom: verticalScale(10),
@@ -9,6 +13,9 @@ export const styles = StyleSheet.create({
   cardForm: {
     height: verticalScale(200),
     marginTop: verticalScale(12),
+  },
+  cardFormContainer: {
+    marginTop: verticalScale(5),
   },
   container: {
     marginHorizontal: horizontalScale(24),
